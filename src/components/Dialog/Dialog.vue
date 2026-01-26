c33c9edabefc229d48297632477b01aa6ec0860d
Kira <hanzomein228@gmail.com>
2026-01-26 16:23:05 +0300

    Сделал модалку

---
+++ src/components/Dialog/Dialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-dialog
+      width="auto"
+      max-width="50%"
+      class="tw-absolute"
+      @click:outside="close"
+      :value="value"
+      :overlay="false"
+      transition="dialog-transition"
+    >
+      <v-card>
+        <v-card-title primary-title> {{ title }} </v-card-title>
+        <v-card-text>
+          <slot name="content" />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text @click="close"> Закрыть </v-btn>
+          <v-btn color="blue" text @click="close"> Скачать </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script lang="ts" setup>
+interface DialogProps {
+  value?: boolean;
+  title?: string;
+}
+const props = defineProps<DialogProps>();
+
+const emit = defineEmits<{
+  (e: "input", val: boolean): void;
+}>();
+
+const updateVisibility = (val: boolean) => {
+  emit("input", val);
+};
+const close = () => {
+  emit("input", false);
+};
+</script>
