329bf88da436eb025629aaf21ad9592f1c510d53
Kira <hanzomein228@gmail.com>
2026-02-03 13:11:02 +0300

    убрал див у слекта

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -1,35 +1,33 @@
 <template>
-  <div class="tw-flex tw-items-center">
-    <v-select
-      :label="label"
-      :placeholder="placeholder"
-      :items="items"
-      :hide-details="true"
-      :item-text="itemText"
-      :item-value="itemValue"
-      :return-object="returnObject"
-      dense
-      color="primary"
-      :multiple="multiple"
-      deletable-chips
-      @input="handleSelect"
-      :value="value"
-      :clearable="clearable"
-    >
-      <template v-if="chips" v-slot:selection="{ item, index }">
-        <slot name="selectedChip" :item="item" :index="index"></slot>
-      </template>
-      <template v-if="customList" v-slot:item="{ item, on, attrs }">
-        <slot :item="item" :on="on" :attrs="attrs" name="ui-item">
-          <v-list-item v-bind="attrs" v-on="on">
-            <v-list-item-content>
-              <v-list-item-title>{{ item.label }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </slot>
-      </template>
-    </v-select>
-  </div>
+  <v-select
+    :label="label"
+    :placeholder="placeholder"
+    :items="items"
+    :hide-details="true"
+    :item-text="itemText"
+    :item-value="itemValue"
+    :return-object="returnObject"
+    dense
+    color="primary"
+    :multiple="multiple"
+    deletable-chips
+    @input="handleSelect"
+    :value="value"
+    :clearable="clearable"
+  >
+    <template v-if="chips" v-slot:selection="{ item, index }">
+      <slot name="selectedChip" :item="item" :index="index"></slot>
+    </template>
+    <template v-if="customList" v-slot:item="{ item, on, attrs }">
+      <slot :item="item" :on="on" :attrs="attrs" name="ui-item">
+        <v-list-item v-bind="attrs" v-on="on">
+          <v-list-item-content>
+            <v-list-item-title>{{ item.label }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </slot>
+    </template>
+  </v-select>
 </template>
 
 <script lang="ts" setup generic="T, H">
